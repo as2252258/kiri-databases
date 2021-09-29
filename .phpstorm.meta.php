5ec3ca65e14517b8cd401027ed37a65a561b62e9
Mr·x <as2252258@163.com>
2021-09-29 17:32:56 +0800

    改名

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -6,7 +6,7 @@ namespace PHPSTORM_META {
 	use Kiri\Di\Container;
 
 	override(Container::get(0), map('@'));
-	override(Container::newObject(0), map('@'));
+	override(Container::create(0), map('@'));
 //    override(\Hyperf\Utils\Context::get(0), map('@'));
 //    override(\make(0), map('@'));
     override(\di(0), map('@'));
