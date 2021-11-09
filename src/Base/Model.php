b3d1f642413b04f3bd477d618790493e08085b2d
Administrator <as2252258@163.com>
2021-11-10 02:42:52 +0800

    1

--- src/Base/Model.php
+++ src/Base/Model.php
@@ -540,9 +540,9 @@ abstract class Model extends Component implements ModelInterface, ArrayAccess, T
 	{
 		[$sql, $param] = SqlBuilder::builder(static::query())->insert($param);
 		$dbConnection = $this->getConnection()->createCommand($sql, $param);
-		if (!($lastId = $dbConnection->save(true))) {
-			throw new Exception('保存失败.');
-		}
+
+        $lastId = $dbConnection->save(true);
+
 		$lastId = $this->setPrimary((int)$lastId, $param);
 
 		$this->refresh()->afterSave($attributes, $param);
