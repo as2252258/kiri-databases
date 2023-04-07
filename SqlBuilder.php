b2807ffe6f4b18656ce22c153a61266b773262e0
向林 <as2252258@163.com>
2023-04-07 18:50:26 +0800

    变更

--- SqlBuilder.php
+++ SqlBuilder.php
@@ -248,9 +248,6 @@ class SqlBuilder extends Component
 		if (($condition = $this->conditionToString()) != '') {
 			$select .= " WHERE $condition";
 		}
-//		if (count($this->query->attributes) > 0) {
-//			$select = strtr($select, $this->query->attributes);
-//		}
 		if ($this->query->group != "") {
 			$select .= ' GROUP BY ' . $this->query->group;
 		}
