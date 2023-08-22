2e01419960a120356e75b1718d00fb70ace907fd
向林 <as2252258@163.com>
2023-08-23 01:48:34 +0800

    qqq

--- Connection.php
+++ Connection.php
@@ -348,11 +348,6 @@ class Connection extends Component
             PDO::ATTR_TIMEOUT            => $this->connect_timeout,
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES ' . $this->charset
         ];
-//        if (!Context::inCoroutine()) {
-//            $options[PDO::ATTR_PERSISTENT] = true;
-//        } else {
-//            $options[PDO::ATTR_PERSISTENT] = false;
-//        }
         $link = new PDO('mysql:dbname=' . $this->database . ';host=' . $this->cds, $this->username, $this->password, $options);
         foreach ($this->attributes as $key => $attribute) {
             $link->setAttribute($key, $attribute);
